a6bbd7f4a0c8eba3560bfccb4383aca3af5844b7
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-03-01 12:59:59 -0500

    Create basic functionality in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
+import ScoreList from "./scorelist";
 
+
+const leaderboard = new ScoreList();
+
+window.onload = () => {
+  document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    leaderboard.addScore();
+    console.log('button works')
+  });
+};
 
